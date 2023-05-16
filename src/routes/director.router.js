2b1f9e8e5b654d86f9a69d0639825871b1217c00
Jeffield97 <jchariguaman@gmail.com>
2023-05-15 22:21:32 -0500

    router cambiado

--- src/routes/director.router.js
+++ src/routes/director.router.js
@@ -1,4 +1,4 @@
-const { getAll, create, getOne, remove, update } = require('../controllers/genre.controller');
+const { getAll, create, getOne, remove, update } = require('../controllers/director.controller');
 const express = require('express');
 
 const Director = express.Router();
